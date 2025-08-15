6ede5730f05d9f55491a49147826279982115f1d
Chase Bradley <bradleycha@proton.me>
2025-08-15 00:17:42 -0400

    Create prettier name and description

--- rcon.lua
+++ rcon.lua
@@ -1,5 +1,5 @@
--- name: Remote Console
--- description: Execute commands as the host remotely.
+-- name: \\#808080\\Remote \\#f08080\\Console
+-- description: \\#808080\\Remote \\#f08080\\Console\\#ffffff\\ \\#f0f0a0\\v1.0.0\\#ffffff\\ by Chase Bradley (\\#fb7d7d\\BLJIntoYourHeart\\#ffffff\\)\n\nProvides commands to remotely send commands and chat messages from the host as an average player.  Type '\\#808080\\/rcon help\\#ffffff\\' for usage.\n\n For more information, please visit the mod's \\#606060\\GitHub\\#ffffff\\ repository at \\#9090ff\\https://github.com/bradleycha/sm64coopdx-mod-rcon\\#ffffff\\
 
 local function rcon_text_info(message)
    local color = "\\#90f090\\"
